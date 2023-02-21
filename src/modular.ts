cdc295fe8425ac0867375130516144bc5b95f864
Vladimir Klepov <vklepov@ozon.ru>
2023-02-21 19:34:37 +0100

    support removeItem

--- src/modular.ts
+++ src/modular.ts
@@ -1,21 +1,24 @@
 const id = <T>(v: T) => v;
 
-export function safeStore<T>(storage: TypedStorage<T>): SafeStore<T> {
-  return {
+export function safeStore<T>(storage: TypedStorage<T>) {
+  const store: SafeStore<T> = {
     getItem: storage.getItem.bind(storage),
     setItem: storage.setItem.bind(storage),
+    removeItem: storage.removeItem.bind(storage),
     singleton: (key) => ({
       getItem: storage.getItem.bind(storage, key),
       setItem: storage.setItem.bind(storage, key),
+      removeItem: storage.removeItem.bind(storage, key),
     }),
     format: (options) =>
       safeStore({
         getItem: (key) => (options.parse || id)(storage.getItem(key)),
-        setItem: (key, value) =>
-          storage.setItem(key, (options.prepare || id)(value)),
+        setItem: (key, value) => storage.setItem(key, (options.prepare || id)(value)),
+        removeItem: store.removeItem,
       }),
-    use: (options) => safeStore(options(storage)),
+    use: (options) => safeStore(options(store)),
   };
+  return store;
 }
 
 export const json = () => ({
@@ -27,11 +30,13 @@ export function scope<T>(prefix: string): Plugin<T, T> {
   return (storage) => ({
     getItem: (key) => storage.getItem(prefix + ":" + key),
     setItem: (key, value) => storage.setItem(prefix + ":" + key, value),
+    removeItem: key => storage.removeItem(prefix + ":" + key),
   });
 }
 
 export function safeGet<T>(fallback: () => T): Plugin<T, T> {
   return (store) => ({
+    ...store,
     getItem: (key) => {
       try {
         return store.getItem(key);
@@ -39,13 +44,12 @@ export function safeGet<T>(fallback: () => T): Plugin<T, T> {
         return fallback();
       }
     },
-    setItem: store.setItem,
   });
 }
 
 export function safeSet<T>(): Plugin<T, T> {
   return (store) => ({
-    getItem: store.getItem,
+    ...store,
     setItem: (key, value) => {
       try {
         store.setItem(key, value);
@@ -71,6 +75,7 @@ export interface SafeStore<T> extends TypedStorage<T> {
 interface TypedStorage<ItemType> {
   getItem: (key: string) => ItemType;
   setItem: (key: string, value: ItemType) => void;
+  removeItem: (key: string) => void;
 }
 
 export type Formatter<Inner, Outer> = ([Inner] extends [Outer]
@@ -80,11 +85,12 @@ export type Formatter<Inner, Outer> = ([Inner] extends [Outer]
     ? { prepare?: (rawValue: Outer) => Inner }
     : { prepare: (rawValue: Outer) => Inner });
 
-export type Plugin<In, Out> = (store: TypedStorage<In>) => TypedStorage<Out>;
+export type Plugin<In, Out> = (store: SafeStore<In>) => TypedStorage<Out>;
 
 export interface SingletonStore<T> {
   getItem: () => T;
   setItem: (value: T) => void;
+  removeItem: () => void;
 }
 
 export function banditStash<T>(options) {
@@ -97,3 +103,11 @@ export function banditStash<T>(options) {
   options.scope && (store = store.use(scope(options.scope)));
   return store;
 }
+
+window['ds'] = safeStore(localStorage).format(json()).format<number>({
+  parse: x => typeof x === 'number' ? x : fail(),
+  prepare: y => y,
+})
+.use(safeGet(() => 0))
+.use(safeSet())
+.use(scope('app'));
