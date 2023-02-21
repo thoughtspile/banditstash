89d834b0a1a27f4c72f8f97347df07f05651598d
Vladimir Klepov <vklepov@ozon.ru>
2023-02-21 16:28:16 +0100

    fully modular

--- src/modular.ts
+++ src/modular.ts
@@ -1,47 +1,51 @@
-export function safeStore<Storage>(options: SafeStoreOptions<Storage>) {
-  const { storage, scope, safeSet = true } = options;
-  const getKey = (key: string) => (scope ? scope + ":" + key : key);
-  const store: SafeStore<Storage> = {
-    getItem: (key) => storage.getItem(getKey(key)),
-    setItem: (key, value) => {
-      try {
-        storage.setItem(getKey(key), value);
-      } catch (err) {
-        if (!safeSet) throw err;
-      }
-    },
+export function safeStore<StorageType>(storage: TypedStorage<StorageType>) {
+  const store: SafeStore<StorageType> = {
+    getItem: storage.getItem.bind(storage),
+    setItem: storage.setItem.bind(storage),
     singleton: (key) => ({
       getItem: store.getItem.bind(null, key),
       setItem: store.setItem.bind(null, key),
     }),
-    scope: (prefix) => safeStore({ ...options, scope: getKey(prefix) }),
-    use: ({ parse, prepare, fallback }) =>
-      safeStore({
-        ...options,
-        storage: {
-          getItem: (key) => {
-            try {
-              return parse(store.getItem(key));
-            } catch (err) {
-              if (fallback) return fallback();
-              throw err;
-            }
-          },
-          setItem: (key, value) => store.setItem(key, prepare(value)),
-        },
-      }),
+    use: (options) => safeStore(typeof options === 'function' ? options(store) : {
+      getItem: (key) => options.parse(store.getItem(key)),
+      setItem: (key, value) => store.setItem(key, options.prepare(value)),
+    }),
   };
   return store;
 }
 
 export const json = () => ({
   parse: (raw: string | null): Json =>
-    raw === null ? safeStore.fail() : JSON.parse(raw),
+    raw === null ? fail() : JSON.parse(raw),
   prepare: (data: Json) => JSON.stringify(data),
-  fallback: false as const,
 });
 
-safeStore.fail = function fail(message?: string): never {
+export const scope = <T>(prefix: string): Plugin<T, T> => (storage) => ({
+  getItem: key => storage.getItem(prefix + ':' + key),
+  setItem: (key, value) => storage.setItem(prefix + ':' + key, value),
+});
+
+export const safeGet = <T>(fallback: false | (() => T)): Plugin<T, T> => store => ({
+  getItem: fallback ? (key) => {
+    try {
+      return store.getItem(key);
+    } catch (err) {
+      return fallback();
+    }
+  } : store.getItem,
+  setItem: store.setItem,
+});
+
+export const safeSet = <T>(): Plugin<T, T> => store => ({
+  getItem: store.getItem,
+  setItem: (key, value) => {
+    try {
+      store.setItem(key, value);
+    } catch (err) {}
+  }
+});
+
+export const fail = function fail(message?: string): never {
   throw new TypeError(message || "BanditStore parse failed");
 };
 
@@ -49,30 +53,35 @@ safeStore.fail = function fail(message?: string): never {
 type Primitive = string | number | boolean | null | undefined;
 export type Json = Primitive | Json[] | { [key: string]: Json };
 
-export interface SafeStoreOptions<StorageType> {
-  storage: {
-    getItem: (key: string) => StorageType;
-    setItem: (key: string, value: StorageType) => void;
-  };
-  safeSet?: boolean;
-  scope?: string;
-}
+type TypedStorage<ItemType, Keys = string> = {
+  getItem: [Keys] extends [never] ? () => ItemType : (key: string) => ItemType;
+  setItem: [Keys] extends [never] ? (value: ItemType) => void : (key: string, value: ItemType) => void;
+};
 
 export interface ParserOptions<Outer, Inner> {
-  fallback: false | (() => Outer);
   parse: (rawValue: Inner) => Outer;
   prepare: (rawValue: Outer) => Inner;
 }
 
-export interface SafeStore<T> {
-  getItem: (key: string) => T;
-  setItem: (key: string, value: T) => void;
+type Plugin<In, Out> = (store: SafeStore<In>) => TypedStorage<Out>;
+
+export interface SafeStore<T> extends TypedStorage<T> {
   singleton: <Key extends string>(key: Key) => SingletonStore<T>;
-  scope: (prefix: string) => SafeStore<T>;
-  use: <Outer>(options: ParserOptions<Outer, T>) => SafeStore<Outer>;
+  use: <Outer>(options: Plugin<T, Outer> | ParserOptions<Outer, T>) => SafeStore<Outer>;
 }
 
 export interface SingletonStore<T> {
   getItem: () => T;
   setItem: (value: T) => void;
 }
+
+
+window['ds'] = safeStore(localStorage)
+    .use(json())
+    .use<number>({
+      parse: data => typeof data === 'number' ? data : fail(),
+      prepare: raw => raw
+    })
+    .use(safeGet(() => 0))
+    .use(scope('app'))
+    .use(safeSet());
--- src/tests/memoryStorgage.ts
+++
@@ -1,8 +0,0 @@
-export function makeMemoryStorage() {
-  const storageData = new Map<string, string>();
-  const storage = {
-    getItem: (key: string) => storageData.get(key) ?? null,
-    setItem: (key: string, value: string) => storageData.set(key, value),
-  };
-  return { storage, storageData };
-}
